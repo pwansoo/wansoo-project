47d4b0b079ef46d5c120918ebc4d4b42d1139011
wansoo park <ceolren@naver.com>
2024-03-11 19:10:51 +0900

    first commit

---
+++ file.py
@@ -0,0 +1,10 @@
+with open("new_file.txt", "r") as file:
+    file.write("Data = 1 \n")
+    file.write("Data = 2 \n") 
+    file.write("Data = 3")
+
+print(file)
+
+
+
+
---
+++ mission_1.py
@@ -0,0 +1,5 @@
+file = open("mission_1.txt", "r")
+
+
+
+file.close()
